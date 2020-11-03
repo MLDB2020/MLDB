9450b0281650ae68862b67cee806070e08b86969
rmartins85 <50893308+rmartins85@users.noreply.github.com>
2020-11-02 19:18:16 -0500

    creating login page

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import NavBar from './NavBar';
+import '../styles/App.css';
+import SignIn from './SignIn';
+
+function App() {
+  return (
+    <div className="App">
+        <NavBar />
+    </div>
+  );
+}
+
+export default App;
