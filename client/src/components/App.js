ac1c461acb076fb00d10dd2741c4295e83e0fc2c
rmartins85 <50893308+rmartins85@users.noreply.github.com>
2020-11-11 14:59:30 -0500

    changing the design and adding routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,17 +1,26 @@
 import React from 'react';
-//import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import NavBar from './NavBar';
 import '../styles/App.css';
 import SignIn from './SignIn';
 import Register from './Register';
+import NoMatch from './NoMatch';
+import Layout from './Layout';
 
 function App() {
   return (
-    <div className="App">
+    <React.Fragment>
         <NavBar />
-        <SignIn />
-        <Register />
-    </div>
+        <Layout>
+            <Router>
+                <Switch>
+                    <Route path="/signin" component={ SignIn }/>
+                    <Route path="/register" component={ Register }/>
+                    <Route path="/nomatch" component={ NoMatch }/>
+                </Switch>
+            </Router>
+        </Layout>
+    </React.Fragment>
   );
 }
 
---
+++ client/src/components/NoMatch.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NoMatch = () => (
+    <div>
+        <h2>No Match</h2>
+    </div>
+);
+
+export default NoMatch;
+
+
+
