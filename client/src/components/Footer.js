20aaf3a627ffcabe5e32eadeb16bc56782f0c1fa
rodrigo <rodrigo_rmartins@outlook.com>
2021-03-11 17:58:04 -0500

    making footer year automatic

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react'
 
 function Footer() {
+	const year = new Date().getFullYear();
+
 	return (
 		<div className="footer__container" id="footer">
-			<p>&#169;Copyright - MLDB 2020</p>
+			<p>&#169;Copyright - MLDB { year }</p>
 		</div>
 	)
 }
